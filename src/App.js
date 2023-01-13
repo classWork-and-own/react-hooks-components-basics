18703b94946353c05d1af2480198cb44984e6552
mukandalee <leonardmukanda254@gmail.com>
2023-01-13 04:05:04 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
+// parent  function component
 function App() {
   return (
     <div>
+      {/* making use of the child components in this parent component */}
       <Article />
       <Comment />
     </div>
